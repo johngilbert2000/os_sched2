5c9c3e352b74a4674d79dce26b2ff8e9fd0383aa
John Gilbert <johngilbert2571@gmail.com>
2020-05-24 17:47:41 +0800

    Added kernel files

---
+++ kernel_files/dummy_proc/.built-in.a.cmd
@@ -0,0 +1,1 @@
+cmd_dummy_proc/built-in.a := rm -f dummy_proc/built-in.a; ar cDPrST dummy_proc/built-in.a dummy_proc/dummy_proc.o
---
+++ kernel_files/dummy_proc/dummy_proc.c
@@ -0,0 +1,29 @@
+
+#include <linux/kernel.h>
+#include <linux/syscalls.h>
+#include <linux/ktime.h>
+
+SYSCALL_DEFINE1(dummy_proc, int, times)
+{
+	struct timespec start_time, end_time;
+	volatile int t;
+	volatile long i;
+
+	getnstimeofday(&start_time);
+
+	for (t = 0; t < times; t++) {
+		for (i = 0; i < 1000000UL; i++);
+	}
+
+	getnstimeofday(&end_time);
+
+	printk(
+		"[Project1] %ld %ld.%ld %ld.%ld\n",
+		(long)task_pid_nr(current),
+		(long)start_time.tv_sec,
+		start_time.tv_nsec,
+		(long)end_time.tv_sec,
+		end_time.tv_nsec);
+
+	return 0;
+}
